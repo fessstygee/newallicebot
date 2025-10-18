34b37543e9c53461e17ea878a7d64360b9009e01
Venom Pratap <venompratap22@gmail.com>
2025-10-18 12:19:12 +0530

    Update play.py

--- VenomMusic/plugins/play/play.py
+++ VenomMusic/plugins/play/play.py
@@ -52,7 +52,7 @@ async def play_commnd(
     plist_type = None
     spotify = None
     user_id = message.from_user.id
-    user_name = message.from_user.first_name
+    user_name = message.from_user.mention
     audio_telegram = (
         (message.reply_to_message.audio or message.reply_to_message.voice)
         if message.reply_to_message
@@ -660,3 +660,4 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+
